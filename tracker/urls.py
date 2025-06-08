f21284ba9d67204abc15dc7fb9147922f29c7e5a
flip64 <65002468+flip64@users.noreply.github.com>
2025-06-08 09:52:05 +0330

    Update urls.py
    Edit name https://chek.liara.run/tracker/watched-urls

--- tracker/urls.py
+++ tracker/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('check_price/', check_price, name='check_price'),
-    path('watched-urls/', views.watched_urls_view, name='watched_urls'),
+    path('watched_urls/', views.watched_urls_view, name='watched_urls'),
 
 ]
